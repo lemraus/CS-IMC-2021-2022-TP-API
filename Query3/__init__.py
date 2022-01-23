347fd79caac0abc848f76a893642f8b2789cf753
Armel Chausse <35761548+lemraus@users.noreply.github.com>
2022-01-23 19:19:43 +0100

    Updating query 3

--- Query3/__init__.py
+++ Query3/__init__.py
@@ -1,7 +1,4 @@
 import logging
-from py2neo import Graph
-from py2neo.bulk import create_nodes, create_relationships
-from py2neo.data import Node
 import os
 import pyodbc as pyodbc
 import azure.functions as func
@@ -18,53 +15,33 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
-    
+            
     server = os.environ["TPBDD_SERVER"]
     database = os.environ["TPBDD_DB"]
     username = os.environ["TPBDD_USERNAME"]
     password = os.environ["TPBDD_PASSWORD"]
     driver= '{ODBC Driver 17 for SQL Server}'
 
-    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
-    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
-    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
-
-    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 or len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0:
         return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
         
     errorMessage = ""
     dataString = ""
     try:
-        logging.info("Test de connexion avec py2neo...")
-        graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
-        producers = graph.run("MATCH (n:Artist) RETURN DISTINCT n.idArtist, n.primaryName LIMIT 3")
-        for producer in producers:
-            dataString += f"CYPHER: idArtist={producer['n.idArtist']}, primaryName={producer['n.primaryName']}\n"
+        logging.info("Test de connexion avec pyodbc...")
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+            cursor = conn.cursor()
+            cursor.execute("select top(1) primaryName from tArtist")
 
-        try:
-            logging.info("Test de connexion avec pyodbc...")
-            with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-                cursor = conn.cursor()
-                cursor.execute("SELECT birthYear, COUNT(*) FROM tArtist WHERE birthYear = ( SELECT TOP(1) birthYear FROM tArtist WHERE birthYear != '0' GROUP BY birthYear ORDER BY COUNT(*) DESC ) GROUP BY birthYear")
-
-                rows = cursor.fetchall()
-                for row in rows:
-                    dataString += f"SQL: tconst={row[0]}, primaryTitle={row[1]}, averageRating={row[2]}\n"
-
-
-        except:
-            errorMessage = "Erreur de connexion a la base SQL"
+            rows = cursor.fetchall()
+            dataString += f"Année de naissance la plus représentée et nombre d'artistes étant nés cette année-là :\n{rows[0][0]} {rows[0][1]}\n"
     except:
-        errorMessage = "Erreur de connexion a la base Neo4j"
-        
-    
-    if name:
-        nameMessage = f"Hello, {name}!\n"
-    else:
-        nameMessage = "Le parametre 'name' n'a pas ete fourni lors de l'appel.\n"
-    
-    if errorMessage != "":
-        return func.HttpResponse(dataString + nameMessage + errorMessage, status_code=500)
+        errorMessage = "Erreur de connexion a la base SQL"
 
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.\n\n{dataString}{errorMessage}")
     else:
-        return func.HttpResponse(dataString + nameMessage + " Connexions réussies a Neo4j et SQL!")
+        return func.HttpResponse(
+             f"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.\n\n{dataString}{errorMessage}",
+             status_code=200
+        )
