24872a980463b5f98c8c4178492600dd6e87ec66
Armel Chausse <35761548+lemraus@users.noreply.github.com>
2022-01-23 18:45:59 +0100

    Updating query 3

--- Query3/__init__.py
+++ Query3/__init__.py
@@ -30,14 +30,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         logging.info("Test de connexion avec pyodbc...")
         with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-            try:
-                cursor = conn.cursor()
-                cursor.execute("select birthYear, count(*) from tArtist where birthYear = ( select top 1 birthYear from tArtist where birthYear != '0' group by birthYear order by count(*) desc ) group by birthYear")
+            cursor = conn.cursor()
+            cursor.execute("select birthYear, count(*) from tArtist where birthYear = ( select top 1 birthYear from tArtist where birthYear != '0' group by birthYear order by count(*) desc ) group by birthYear")
 
-                rows = cursor.fetchall()
-                dataString += f"Année de naissance la plus représentée et nombre d'artistes étant nés cette année-là :\n{rows[0][0]} {rows[0][1]}\n"
-            except Exception as e:
-                print(e)
+            rows = cursor.fetchall()
+            dataString += f"Année de naissance la plus représentée et nombre d'artistes étant nés cette année-là :\n{rows[0][0]} {rows[0][1]}\n"
     except:
         errorMessage = "Erreur de connexion a la base SQL"
 
